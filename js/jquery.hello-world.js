b0c04f032e26524947582b3fb0014134ff1ed45b
aaVIaDoR io <vid.repar@gmail.com>
2016-04-14 13:35:18 +0200

    Optimized the plugin and added some descriptive comments for better understanding

--- js/jquery.hello-world.js
+++ js/jquery.hello-world.js
@@ -1,13 +1,38 @@
 //by including everything in the (function() {}) self-enclosed JavaScript pattern, we’re making sure that all the variables in our plugin will stay safely outside of the global namespace
-(function($) {
+(function ($) {
 
-  //$.fn is jQuery’s way of allowing you to define your plugin, which we’ve named helloWorld
-  $.fn.helloWorld = function( customText ) {
+//$.fn is jQuery’s way of allowing you to define your plugin, which we’ve named helloWorld
+  $.fn.helloWorld = function (options) {
 
-    // Future home of "Hello, World!"
-    //change every 'this' with Hello, World!
-    this.each(function(){
-      return $(this).text( customText );
+    //Establish our default settings
+    var settings = $.extend({
+
+      text      : 'Hello, World!',
+      color     : null,
+      fontStyle : null,
+
+      //complete = callback, we see our callback as our plugin is executed
+      complete  : null
+
+    }, options);
+
+    //loops through all elemets that helloWorld plugin targets
+    return this.each(function () {
+
+      $(this).text(settings.text);
+
+      if (settings.color) {
+        $(this).css('color', settings.color);
+      }
+
+      if (settings.fontStyle) {
+        $(this).css('font-style', settings.fontStyle);
+      }
+
+      if ($.isFunction(settings.complete)) {
+        settings.complete.call(this);
+      }
+
     });
 
   }
